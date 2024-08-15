03fc348c877e2cf6a1be0f5f3b4e65d2aad099ae
devcui <devcui@outlook.com>
2024-08-15 15:33:34 +0800

    feat: add endpoints

---
+++ server/routes/token/introspect.post.ts
@@ -0,0 +1,12 @@
+import { useOAuthServer } from '../../composables/oauth/use_oauth_server'
+import { requestFromH3, handleErrorWithH3, responseWithH3 } from '../../tools/converts'
+
+export default defineEventHandler(async (event) => {
+  try {
+    const oauthServer = useOAuthServer()
+    const oauthResponse = await oauthServer.introspect(await requestFromH3(event))
+    responseWithH3(event, oauthResponse)
+  } catch (e) {
+    handleErrorWithH3(event, e)
+  }
+})
